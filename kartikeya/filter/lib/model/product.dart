8ee6123cd49dd1ad9d3c5eb0549c67e47c9c7180
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-09-23 01:45:31 +0530

    Basic Layout created  And drop down list Added

---
+++ kartikeya/filter/lib/model/product.dart
@@ -0,0 +1,37 @@
+class Product {
+  final String name;
+  final String category;
+  final int price;
+
+  Product({
+    required this.name,
+    required this.category,
+    required this.price,
+  });
+}
+
+final List<Product> productList = [
+  Product(name: 'Sandwich', category: 'Food',price: 20),
+    Product(name: 'Apple', category: 'Fruit', price: 10),
+  Product(name: 'Banana', category: 'Fruit', price: 15),
+  Product(name: 'Orange', category: 'Fruit', price: 20),
+  Product(name: 'Grapes', category: 'Fruit', price: 25),
+  Product(name: 'Mango', category: 'Fruit', price: 30),
+  Product(name: 'Pizza', category: 'Food', price: 100),
+  Product(name: 'Burger', category: 'Food', price: 80),
+  Product(name: 'Pasta', category: 'Food', price: 120),
+  Product(name: 'Noodles', category: 'Food', price: 70),
+  Product(name: 'Ice Cream', category: 'Food', price: 50),
+  Product(name: 'Water', category: 'Drink', price: 10),
+  Product(name: 'Juice', category: 'Drink', price: 30),
+  Product(name: 'Soda', category: 'Drink', price: 20),
+  Product(name: 'Coffee', category: 'Drink', price: 40),
+  Product(name: 'Tea', category: 'Drink', price: 20),
+  Product(name: 'Strawberry Shake', category:'Drink' ,price :60 ),
+  Product(name:'Pineapple Shake' ,category:'Drink' ,price :70 ),
+  Product(name:'Mango Shake' ,category:'Drink' ,price :80 ),
+  Product(name:'Chocolate Shake' ,category:'Drink' ,price :90 ),
+  Product(name:'Vanilla Shake' ,category:'Drink' ,price :100 ),
+];
+ 
+
