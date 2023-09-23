05ab572df0d5def92ed874e3726b43667cf44540
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-09-23 12:56:02 +0530

    List of names and search bar added

--- kartikeya/filter/lib/main.dart
+++ kartikeya/filter/lib/main.dart
@@ -1,10 +1,10 @@
- 
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
+import 'package:filter/model/product.dart'; // Import your data from product.dart
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +12,23 @@ class MyApp extends StatelessWidget {
       title: 'Filter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.indigo),
-      home: const ProductListPage(),
+      home: ProductListPage(),
     );
   }
 }
 
 class ProductListPage extends StatefulWidget {
-  const ProductListPage({super.key});
+  ProductListPage({Key? key}) : super(key: key);
+
   @override
   State<ProductListPage> createState() => _ProductListPageState();
 }
 
- class _ProductListPageState extends State<ProductListPage> {
-  final List<String> categories = ['Food', 'Fruit', 'Drink'];
+class _ProductListPageState extends State<ProductListPage> {
+  final List<String> categories = ['NAME', 'DOB', 'AGE'];
   String? selectedCategory;
+
+  List<String> selectedCategories = [];
 
   @override
   Widget build(BuildContext context) {
@@ -33,34 +36,109 @@ class ProductListPage extends StatefulWidget {
       appBar: AppBar(
         title: const Text('SORT AND FILTER'),
         centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(12.0),
-            margin: const EdgeInsets.all(16.0),
-            child: DropdownButton<String>(
-           
-              hint: const Text('Select Category' , style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),)),
-              value: selectedCategory,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedCategory = newValue;
-                });
-              },
-              items: categories.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-       
-       
-
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: DataSearch(personList: personList),
+              );
+            },
+          )
         ],
       ),
+      body: ListView.builder(
+        itemCount: personList.length,
+        itemBuilder: (context, index) {
+          final person = personList[index];
+          return ListTile(
+            title: Text(person.name),
+            // You can add more details here if needed.
+            // subtitle: Text("DOB: ${person.dateOfBirth}, Age: ${person.age}"),
+            onTap: () {
+              // Handle when a person is tapped.
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  final List<Person> personList;
+
+  DataSearch({required this.personList});
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final filteredPersons = personList
+        .where((person) =>
+            person.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+      itemCount: filteredPersons.length,
+      itemBuilder: (context, index) {
+        final person = filteredPersons[index];
+        return ListTile(
+          title: Text(person.name),
+          onTap: () {
+            // Handle when a person is tapped.
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? personList.map((person) => person.name).toList()
+        : personList
+            .where((person) =>
+                person.name.toLowerCase().contains(query.toLowerCase()))
+            .map((person) => person.name)
+            .toList();
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        final suggestion = suggestionList[index];
+        return ListTile(
+          title: Text(suggestion),
+          onTap: () {
+            query = suggestion;
+            showResults(context);
+          },
+        );
+      },
     );
   }
 }
--- kartikeya/filter/lib/model/product.dart
+++ kartikeya/filter/lib/model/product.dart
@@ -1,37 +1,34 @@
-class Product {
+ class Person {
   final String name;
-  final String category;
-  final int price;
+  final String dateOfBirth;
+  final int age;
 
-  Product({
+  Person({
     required this.name,
-    required this.category,
-    required this.price,
+    required this.dateOfBirth,
+    required this.age,
   });
 }
 
-final List<Product> productList = [
-  Product(name: 'Sandwich', category: 'Food',price: 20),
-    Product(name: 'Apple', category: 'Fruit', price: 10),
-  Product(name: 'Banana', category: 'Fruit', price: 15),
-  Product(name: 'Orange', category: 'Fruit', price: 20),
-  Product(name: 'Grapes', category: 'Fruit', price: 25),
-  Product(name: 'Mango', category: 'Fruit', price: 30),
-  Product(name: 'Pizza', category: 'Food', price: 100),
-  Product(name: 'Burger', category: 'Food', price: 80),
-  Product(name: 'Pasta', category: 'Food', price: 120),
-  Product(name: 'Noodles', category: 'Food', price: 70),
-  Product(name: 'Ice Cream', category: 'Food', price: 50),
-  Product(name: 'Water', category: 'Drink', price: 10),
-  Product(name: 'Juice', category: 'Drink', price: 30),
-  Product(name: 'Soda', category: 'Drink', price: 20),
-  Product(name: 'Coffee', category: 'Drink', price: 40),
-  Product(name: 'Tea', category: 'Drink', price: 20),
-  Product(name: 'Strawberry Shake', category:'Drink' ,price :60 ),
-  Product(name:'Pineapple Shake' ,category:'Drink' ,price :70 ),
-  Product(name:'Mango Shake' ,category:'Drink' ,price :80 ),
-  Product(name:'Chocolate Shake' ,category:'Drink' ,price :90 ),
-  Product(name:'Vanilla Shake' ,category:'Drink' ,price :100 ),
+final List<Person> personList = [
+  Person(name: 'John Doe', dateOfBirth: '1990-01-01', age: 33),
+  Person(name: 'Jane Smith', dateOfBirth: '1985-02-02', age: 38),
+  Person(name: 'Robert Johnson', dateOfBirth: '1992-03-03', age: 31),
+  Person(name: 'Michael Brown', dateOfBirth: '1980-04-04', age: 43),
+  Person(name: 'Emily Davis', dateOfBirth: '1995-05-05', age: 28),
+  Person(name: 'Sarah Miller', dateOfBirth: '1993-06-06', age: 30),
+  Person(name: 'Jessica Wilson', dateOfBirth: '1988-07-07', age: 35),
+  Person(name: 'Mohammed Patel', dateOfBirth: '1991-08-08', age: 32),
+  Person(name: 'Sophia Taylor', dateOfBirth: '1987-09-09', age: 36),
+  Person(name: 'Jacob Thomas', dateOfBirth: '1996-10-10', age: 27),
+  Person(name: 'Mia Jackson', dateOfBirth: '1989-11-11', age: 34),
+  Person(name: 'Lucas White', dateOfBirth: '1994-12-12', age: 29),
+  Person(name: 'Grace Harris', dateOfBirth: '1997-01-13', age: 26),
+  Person(name: 'James Martin', dateOfBirth: '1986-02-14', age: 37),
+  Person(name: 'Oliver Thompson', dateOfBirth:'1984-03-15' ,age :39 ),
+  Person(name:'Amelia Garcia' ,dateOfBirth:'1998-04-16' ,age :25 ),
+  Person(name:'Muhammad Martinez' ,dateOfBirth:'1983-05-17' ,age :40 ),
+  Person(name:'Freddie Robinson' ,dateOfBirth:'1982-06-18' ,age :41 ),
+  Person(name:'Poppy Clark' ,dateOfBirth:'1999-07-19' ,age :24 ),
+  Person(name:'Willow Rodriguez' ,dateOfBirth:'2000-08-20' ,age :23 ),
 ];
- 
-
