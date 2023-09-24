550c60ea9de26131bef08b6686c1be76864d07a3
Jayraj <jayrajsinhzala1024@gmail.com>
2023-09-24 17:21:12 +0530

    Updated main.dart and pubspec.yaml

--- JayrajSinhZala/first_assignment/lib/main.dart
+++ JayrajSinhZala/first_assignment/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:first_assignment/const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
@@ -10,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,7 +32,6 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   bool isPressed = false;
-
   List persons = [];
   List original = [];
   TextEditingController txtQuery = TextEditingController();
@@ -116,96 +113,171 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: lightblue1,
+      backgroundColor: Colors.lightBlue[100],
       appBar: AppBar(
-        backgroundColor: lightblue1,
+        backgroundColor: Colors.lightBlue[100],
         title: const Text(
           "\tFirst_Assignment",
           style: TextStyle(fontWeight: FontWeight.w600, letterSpacing: 1),
         ),
         actions: [
           PopupMenuButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              onSelected: (value) => onSelected(context, value),
-              itemBuilder: (context) => [
-                    const PopupMenuItem<int>(
-                      value: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("Sort by Name"),
-                          Icon(Icons.sort_by_alpha_rounded)
-                        ],
-                      ),
-                    ),
-                    const PopupMenuItem<int>(
-                      value: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("Sort by Date"),
-                          Icon(Icons.date_range_rounded)
-                        ],
-                      ),
-                    ),
-                  ])
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            onSelected: (value) => onSelected(context, value),
+            itemBuilder: (context) => [
+              const PopupMenuItem<int>(
+                value: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Sort by Name"),
+                    Icon(Icons.sort_by_alpha_rounded)
+                  ],
+                ),
+              ),
+              const PopupMenuItem<int>(
+                value: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Sort by Date"),
+                    Icon(Icons.date_range_rounded)
+                  ],
+                ),
+              ),
+            ],
+          )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            // Landscape mode, use a two-column layout
+            return Row(
               children: [
-                const Text(
-                  "\tSearch & Sort Function",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                  child: TextFormField(
-                    controller: txtQuery,
-                    onChanged: search,
-                    decoration: InputDecoration(
-                      hintText: "Search",
-                      border: OutlineInputBorder(
-                        borderSide: const BorderSide(color: lightblue2),
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        borderSide: const BorderSide(color: lightblue5),
-                      ),
-                      prefixIcon: const Icon(Icons.search),
-                      suffixIcon: txtQuery.text.isEmpty
-                          ? const SizedBox.shrink()
-                          : IconButton(
-                              onPressed: () {
-                                txtQuery.text = "";
-                                search(txtQuery.text);
-                              },
-                              icon: Icon(
-                                Icons.clear,
-                                color: Colors.blueGrey.shade700,
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            const Text(
+                              "Search & Sort Function",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 8),
+                              child: TextFormField(
+                                controller: txtQuery,
+                                onChanged: search,
+                                decoration: InputDecoration(
+                                  hintText: "Search",
+                                  border: OutlineInputBorder(
+                                    borderSide:
+                                        const BorderSide(color: Colors.blue),
+                                    borderRadius: BorderRadius.circular(50),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(50),
+                                    borderSide:
+                                        const BorderSide(color: Colors.blue),
+                                  ),
+                                  prefixIcon: const Icon(Icons.search),
+                                  suffixIcon: txtQuery.text.isEmpty
+                                      ? const SizedBox.shrink()
+                                      : IconButton(
+                                          onPressed: () {
+                                            txtQuery.text = "";
+                                            search(txtQuery.text);
+                                          },
+                                          icon: Icon(
+                                            Icons.clear,
+                                            color: Colors.blueGrey.shade700,
+                                          ),
+                                        ),
+                                ),
                               ),
                             ),
-                    ),
+                          ],
+                        ),
+                      ),
+                      // Add filters or other widgets for landscape mode here
+                    ],
                   ),
-                )
+                ),
+                Expanded(
+                  flex: 3,
+                  child: _listView(persons),
+                ),
               ],
-            ),
-          ),
-          _listView(persons),
-        ],
+            );
+          } else {
+            // Portrait mode, use a single-column layout
+            return Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "\tSearch & Sort Function",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 8),
+                        child: TextFormField(
+                          controller: txtQuery,
+                          onChanged: search,
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  const BorderSide(color: Colors.blue),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide:
+                                  const BorderSide(color: Colors.blue),
+                            ),
+                            prefixIcon: const Icon(Icons.search),
+                            suffixIcon: txtQuery.text.isEmpty
+                                ? const SizedBox.shrink()
+                                : IconButton(
+                                    onPressed: () {
+                                      txtQuery.text = "";
+                                      search(txtQuery.text);
+                                    },
+                                    icon: Icon(
+                                      Icons.clear,
+                                      color: Colors.blueGrey.shade700,
+                                    ),
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                _listView(persons),
+              ],
+            );
+          }
+        },
       ),
     );
   }
@@ -213,67 +285,68 @@ class _SearchPageState extends State<SearchPage> {
 
 Widget _listView(persons) {
   return Expanded(
-      child: Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: ListView.builder(
-      itemCount: persons.length,
-      itemBuilder: (context, index) {
-        var person = persons[index];
-        return Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: lightblue3,
-          ),
-          margin: const EdgeInsets.all(10),
-          child: InkWell(
-            onTap: () {
-              //print(person['Name']);
-              showDialog(
-                barrierColor: Colors.black45,
-                context: context,
-                builder: (ctx) => AlertDialog(
-                  title: Text(person['Name']),
-                  content: Text(
-                      "School:  ${person['School']}\n\nDate of joining:  ${person['Date Of Joining']}"),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(ctx).pop();
-                      },
-                      child: const Text("okay"),
-                    ),
-                  ],
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+        itemCount: persons.length,
+        itemBuilder: (context, index) {
+          var person = persons[index];
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.lightBlue[200],
+            ),
+            margin: const EdgeInsets.all(10),
+            child: InkWell(
+              onTap: () {
+                showDialog(
+                  barrierColor: Colors.black45,
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text(person['Name']),
+                    content: Text(
+                        "School:  ${person['School']}\n\nDate of joining:  ${person['Date Of Joining']}"),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop();
+                        },
+                        child: const Text("okay"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.lightBlue[300],
+                  child: Text(
+                    person['Name'][0],
+                    style: TextStyle(color: Colors.lightBlue[800]),
+                  ),
                 ),
-              );
-            },
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: lightblue4,
-                child: Text(
-                  person['Name'][0],
-                  style: const TextStyle(color: lightblue5),
+                title: Text(
+                  person['Name'],
+                  style: const TextStyle(
+                      color: Colors.black87,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
                 ),
-              ),
-              title: Text(
-                person['Name'],
-                style: const TextStyle(
-                    color: Colors.black87,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600),
-              ),
-              subtitle: Text(
-                "School: ${person['School']}",
-                style: const TextStyle(
-                    fontSize: 12, color: Color.fromARGB(255, 67, 87, 97)),
-              ),
-              trailing: Text(
-                "\nDate: ${person['Date Of Joining'].substring(0, 10)}",
-                style: const TextStyle(color: Color.fromARGB(255, 67, 87, 97)),
+                subtitle: Text(
+                  "School: ${person['School']}",
+                  style: const TextStyle(
+                      fontSize: 12, color: Color.fromARGB(255, 67, 87, 97)),
+                ),
+                trailing: Text(
+                  "\nDate: ${person['Date Of Joining'].substring(0, 10)}",
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 67, 87, 97)),
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     ),
-  ));
+  );
 }
