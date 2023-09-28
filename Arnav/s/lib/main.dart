8f77429e889a6db77b30b5c539f3933bf196692a
Arnav <arnavraj815@gmail.com>
2023-09-28 22:15:15 +0530

    Last Commit

--- Arnav/s/lib/main.dart
+++ Arnav/s/lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}):super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,129 +20,145 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Contactinput{
-  String? Contact_name;
-  int? Number;
-  Contactinput(this.Contact_name,this.Number);
+class Student_input {
+  String? id;
+  String? Name;
+  String? School;
+  String? Date_Of_Joining;
+
+  Student_input(this.id, this.Name, this.School, this.Date_Of_Joining);
+
+  factory Student_input.fromJson(Map<String, dynamic> json) {
+    return Student_input(
+      json['id'],
+      json['Name'],
+      json['School'],
+      json['Date_Of_Joining'],
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}): super(key: key);
-
-
-  // final String title;
-
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _searchController = TextEditingController();
+  List<Student_input> Student_List = [];
+  List<Student_input> display_List = [];
 
+  @override
+  void initState() {
+    super.initState();
+    loadStudentData();
+  }
 
-  static List<Contactinput> Contact_List=[
-    Contactinput('Arnav',6208464464,),
-    Contactinput("Pradeep",6209155222,),
-    Contactinput("Rahul",6209156432,),
-    Contactinput("Vikas",6208757567,),
-    Contactinput("Raman",6208787567,),
-    Contactinput("Vishal",6209757567,),
-    Contactinput("Rajesh",6208227567,),
-    Contactinput("Ramesh",6208765567,),
-    Contactinput("yuvraj",6208758567,),
-    Contactinput("Abhishek",6202357567,),
-    Contactinput("ansh",6208734567,),
-    Contactinput("Yash",6203357567,),
-    Contactinput("Abhinav",6201757567,),
-    Contactinput("ashutosh",6208957567,),
-    Contactinput("shubham",6203957567,),
-    Contactinput("rajnish",6208757522,),
-    Contactinput("priyam",6208757357,),
-    Contactinput("golden",6208752267,),
-    Contactinput("deepak",6208757533,),
-    Contactinput("sintu",6208757555,),
-    Contactinput("ankit",6208757500,),
-    Contactinput("anubhav",6208757508,),
-    Contactinput("divyansh",6208757505,),
-
-  ];
-
-  //    create list that are going to display and filter
-  List<Contactinput> display_List=List.from(Contact_List);
-  void updateList(String value){
-    //   this is the function that will filter our list
+  Future<void> loadStudentData() async {
+    final String jsonString = await rootBundle.loadString('assets/students.json');
+    final List<dynamic> jsonData = jsonDecode(jsonString);
+    Student_List = jsonData.map((json) => Student_input.fromJson(json)).toList();
     setState(() {
-      display_List=Contact_List.where((element) => element.Contact_name!.toLowerCase().contains(value.toLowerCase())).toList();
+      display_List = List.from(Student_List);
+    });
+  }
 
+  void updateList(String value) {
 
+    // if (value.trim().isEmpty) {
+    //   setState(() {
+    //     display_List = Student_List;
+    //   });
+    //   return;
+    // }
+
+    setState(() {
+      display_List = Student_List.where((element) =>
+      (element.Name?.toLowerCase() ?? '').contains(value.toLowerCase()) ||
+          (element.id?.toLowerCase() ?? '').contains(value.toLowerCase()) ||
+          (element.School?.toLowerCase() ?? '').contains(value.toLowerCase()) ||
+          (element.Date_Of_Joining?.toLowerCase() ?? '').contains(value.toLowerCase())
+      ).toList();
     });
   }
 
   @override
-  Widget build(BuildContext context) {
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Search"),
+        title: const Text("Search"),
       ),
       backgroundColor: Colors.green,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: TextField(
-
-               onChanged:(value)=> updateList(value) ,
-               style: TextStyle(
-                 color: Colors.white70,
-               ),
-               decoration: InputDecoration(
-                 border: OutlineInputBorder(
-                   borderSide: BorderSide(
-                     color:Colors.black87,
-                   ),
-                       borderRadius: BorderRadius.circular(0.0)
-                 ),
-                 prefixIcon: Icon(Icons.search),
-                 hintText: "search your result"
-               ),
-               ),
-           ),
-          SizedBox(height: 20,),
-
-
-            Expanded(
-                child: ListView.builder(itemBuilder: (context,index){
-                  return ListTile(
-                contentPadding: EdgeInsets.all(8.0),
-                leading:CircleAvatar(
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage('assets/images/boy.jpeg'),
-
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: (value) => updateList(value),
+              style: const TextStyle(
+                color: Colors.white70,
+              ),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Colors.black87,
+                  ),
+                  borderRadius: BorderRadius.circular(0.0),
                 ),
-                title: Text(display_List[index].Contact_name!,style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),),
-                subtitle:Text("${display_List[index].Number!}",style: TextStyle(
-                  color: Colors.amber,
-                ),),
-                trailing:CircleAvatar(
-                    child: Icon(Icons.add)
-                ),
-
-              );
-
-
-            },
-            itemCount: display_List.length,
-
-            )
+                prefixIcon: const Icon(Icons.search),
+                hintText: "search your result",
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  contentPadding: const EdgeInsets.all(8.0),
+                  leading: Text(
+                    display_List[index].School ?? "Unknown", // Default to "Unknown" if null
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  selectedTileColor: Colors.cyanAccent,
+                  title: Text(
+                    display_List[index].Name ?? "Unknown",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    display_List[index].id ?? "Unknown",
+                    style: TextStyle(
+                      color: Colors.amber,
+                    ),
+                  ),
+                  trailing: Text(
+                    display_List[index].Date_Of_Joining ?? "Unknown",
+                    style: TextStyle(
+                      color: Colors.amber,
+                    ),
+                  ),
+                );
+              },
+              itemCount: display_List.length,
             ),
 
-
+          ),
         ],
       ),
     );
